bff8ceb9134515c0b2fd21568160ea5db95e807a
허송이 <song21677@naver.com>
2020-09-28 17:18:06 +0900

    Modified mac.*

--- mac.cpp
+++ mac.cpp
@@ -15,15 +15,3 @@ Mac::Mac(const std::string r) {
     mac[5] = f;
 }
 
-Mac::operator std::string() const {
-    char buf[32]; // enough size
-    sprintf(buf, "%02x:%02X:%02X:%02X:%02X:%02X",
-        mac[0],
-        mac[1],
-        mac[2],
-        mac[3],
-        mac[4],
-        mac[5]);
-    return std::string(buf);
-}
-
--- mac.h
+++ mac.h
@@ -8,11 +8,7 @@ struct Mac{
     static const int SIZE = 6;
 
     Mac() {}
-    Mac(const uint8_t *r) {memcpy(this->mac, r, SIZE);}
     Mac(std::string r);
-
-    operator uint8_t *() const {return const_cast<uint8_t *>(mac);}
-    explicit operator std::string() const;
 
 protected:
     uint8_t mac[SIZE];
